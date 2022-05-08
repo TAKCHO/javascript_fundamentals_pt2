c33d4bc1fe066d72ff890b46f6b64b036858cf05
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-05-08 14:12:11 +0400

    challange

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ console.log(fruitProcessor(2, 3)); */
 
 /* Reviewing Functions */
 
-const calcAge = function (birthyear) {
+/* const calcAge = function (birthyear) {
     return 2022 - birthyear;
 }
 
@@ -126,4 +126,33 @@ const yearsUntilRetirement = function (birthyear, firstName) {
 }
 
 console.log(yearsUntilRetirement(1997, "tako"));
-console.log(yearsUntilRetirement(1950, "tako"));
+console.log(yearsUntilRetirement(1950, "tako")); */
+
+/* Challange */
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win ( ${avgDolphins} vs ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win ( ${avgKoalas} vs ${avgDolphins})`);
+    } else {
+        console.log(`No teams wins`);
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 144);
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
